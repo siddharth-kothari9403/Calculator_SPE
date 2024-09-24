d884edd238ec08f6c43d83aa80d267875c1703a8
siddharth-kothari9403 <siddharthvatps@gmail.com>
2024-09-24 16:45:27 +0530

    Code updated

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,62 +1,84 @@
-import services.CalculatorService;
-import java.util.*;
+import java.util.Scanner;
 
 public class Calculator {
 
-    public static void main(String[] args){
-        System.out.println("Welcome to Calculator SPE Project!");
-        int ch;
+    public static double square_root (double x){
+        if(x < 0){
+            return Double.NaN; // Invalid input
+        }
+        return Math.sqrt(x);
+    }
+
+    public static double factorial(Integer x){
+        if(x < 0){
+            return Double.NaN; // Invalid input
+        }
+        double fact = 1;
+        for(int i = 1; i <= x; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static double natural_log(double x){
+        if(x <= 0){
+            return Double.NaN; // Invalid input
+        }
+        return Math.log(x);
+    }
+
+    public static double exponent(double x, double y){
+        return Math.pow(x,y);
+    }
+
+    public static void main(String args[]){
+
         Scanner sc = new Scanner(System.in);
 
-        CalculatorService calculatorService = new CalculatorService();
+        while(true){
 
-        while (true){
-            System.out.println("--------------------------------------------------");
-            System.out.println("Enter choice: ");
-            System.out.println("1. Square Root of a Decimal Number");
-            System.out.println("2. Factorial of an Integer");
-            System.out.println("3. Natural Logarithm of a Positive Real Number");
-            System.out.println("4. Power of a Number");
-            System.out.println("5. Exit Menu");
-            System.out.println("Enter your choice:");
-            String s = sc.nextLine();
-            ch = Integer.parseInt(s);
-            System.out.println("--------------------------------------------------");
-            if (ch == 1){
-                System.out.println("Enter Positive Real Number for Square Root");
-                s = sc.nextLine();
-                Double d = Double.parseDouble(s);
-                Double res = calculatorService.sqrt(d);
-                System.out.println(res);
-            }else if (ch == 2){
-                System.out.println("Enter Positive Integer for Factorial");
-                s = sc.nextLine();
-                Integer n = Integer.parseInt(s);
-                Integer res = calculatorService.factorial(n);
-                System.out.println(res);
-            }else if (ch == 3){
-                System.out.println("Enter Positive Real Number for Logarithm");
-                s = sc.nextLine();
-                Double d = Double.parseDouble(s);
-                Double res = calculatorService.logarithm(d);
-                System.out.println(res);
-            }else if (ch == 4){
-                System.out.println("Enter Base: ");
-                s = sc.nextLine();
-                Double base = Double.parseDouble(s);
-                System.out.println("Enter Exponent: ");
-                s = sc.nextLine();
-                Double exp = Double.parseDouble(s);
-                Double res = calculatorService.power(base, exp);
-                System.out.println(res);
-            }else if (ch == 5){
-                System.out.println("Exiting");
+            System.out.println("Please enter one of the following options: ");
+            System.out.println("------------------------------------------");
+            System.out.println("1. for square root");
+            System.out.println("2. for factorial");
+            System.out.println("3. for natural log");
+            System.out.println("4. for exponent");
+            System.out.println("5. to exit the menu");
+            System.out.println("------------------------------------------");
+
+            int op = sc.nextInt();
+
+            if(op == 1){
+                System.out.print("Please Enter the number to find the square root of: ");
+                double num1 = sc.nextDouble();
+                double result = square_root(num1);
+                System.out.println("The answer is: " + result);
+            }else if(op == 2){
+                System.out.print("Please Enter the number to find the factorial of: ");
+                Integer num1 = sc.nextInt();
+                double result = factorial(num1);
+                System.out.println("The answer is: " + result);
+            }else if(op == 3){
+                System.out.print("Please Enter the number to find the natural log of: ");
+                double num1 = sc.nextDouble();
+                double result = natural_log(num1);
+                System.out.println("The answer is: " + result);
+            }else if(op == 4){
+                System.out.print("Please Enter the base: ");
+                double base = sc.nextDouble();
+                System.out.print("Please Enter the exponent: ");
+                double exponent = sc.nextDouble();
+                double result = exponent(base,exponent);
+                System.out.println("The answer is: " + result);
+            }else if(op == 5){
                 break;
             }else{
-                System.out.println("Invalid Choice");
+                System.out.println("Please enter a valid option.");
             }
         }
 
         sc.close();
+
     }
+
 }
